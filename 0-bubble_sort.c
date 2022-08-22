4a7283022e41f83427645dfc7c4044ea391861c1
imenmansouri93 <imennmansourii@gmail.com>
2022-08-22 21:13:22 +0100

    buble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,9 +3,14 @@
 void bubble_sort(int *array, size_t size)
 {
 size_t i, j,  tmp;
+if (size <= 1)
+{
+return;
+}
+
 for (i = 0; i < size  -  1; i++)
 {
-for (j = 0; j <= size - i - 1; j++)
+for (j = 0; j <= size - 2; j++)
 {
 if (array[j] > array[j + 1])
 {
