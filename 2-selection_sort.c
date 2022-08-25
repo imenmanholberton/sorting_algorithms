9889bfc29d01b3a532a1543ab271dcec4656262d
imenmansouri93 <imennmansourii@gmail.com>
2022-08-25 14:47:24 +0100

    selectionsort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,25 +1,33 @@
 #include "sort.h"
 /**
  * @brief
- * 
+ *
  */
-void swap (int *x, int *y)
-{
-int temp = *x;
-*x = *y;
-*y = temp;
-}
+
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, jmin;
-for (i = 0; i <= size - 1; i++)
-{
-jmin = i;
-for (j = i+1; j < size; j++)
-if (array[j] < array [jmin])
-jmin = j;
-if (jmin != i)
-swap(&array[jmin], &array[i]);
-print_array(array, size);
-}
+    size_t i, j;
+    int *min = NULL;
+    int *temp = NULL;
+    if (size <= 1)
+    {
+        return;
+    }
+    for (i = 0; i <= size - 1; i++)
+    {
+        *min = array[i];
+        for (j = i + 1; j < size; j++)
+            if (array[j] < *min)
+            {
+                *min = array[j];
+            }
+        if (array[i] != *min)
+        {
+            
+            *temp = array[i];
+            array[i] = *min;
+            *min = *temp;
+            print_array(array, size);
+        }
+    }
 }
